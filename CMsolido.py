41a21a77463f6eb5dfc62178ed32f125849dc097
BScheihing <bruno.scheihing@ing.uchile.cl>
2015-11-13 22:59:46 -0300

    Define funciones para determinar si un punto esta dentro del solido

---
+++ CMsolido.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def in_toro(x, y, z):
+    return z**2 + (np.sqrt(x**2 + y**2) - 3)**2 <= 1
+
+def in_cilindro(x, z):
+    return (x - 2)**2 + z**2 <= 1
