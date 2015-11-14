aa1e53c14189645f450289e8ca8f967ad65bb7f1
BScheihing <bruno.scheihing@ing.uchile.cl>
2015-11-14 14:45:45 -0300

    Grafica histograma funcion distribucion que se busca generar

--- Metropolis.py
+++ Metropolis.py
@@ -2,19 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(347)
-DELTA = 2
+DELTA = 4
 N_puntos = 10000000
 
 def W(x):
-    return 3.5*np.exp(-((x - 3)**2)/3) + 2*np.exp(-((x + 1.5)**2)/0.5)
+    exp1 = 3.5 * np.exp(-(np.power(x - 3, 2))/3)
+    exp2 = 2 * np.exp(-(np.power(x + 1.5, 2))/0.5)
+    return exp1 + exp2
 
 def proposicion(xn):
     r = np.random.uniform(low=-1, high=1)
-    return xp = xn + r*DELTA
+    return xn + r*DELTA
 
-X = np.zeros(N_puntos)
+X = np.zeros(N_puntos + 1)
 n_aceptados = 0
 n = 0
 while n < N_puntos:
@@ -26,3 +29,13 @@ while n < N_puntos:
     else:
         X[n+1] = X[n]
     n += 1
+
+x_w = np.linspace(-4, 8, 10000)
+I = np.sqrt(np.pi) * (3.5*np.sqrt(3) + 2*np.sqrt(0.5))
+print "Numero de proposiciones aceptadas: ", n_aceptados
+
+plt.clf()
+plt.figure(1)
+plt.hist(X, bins=100, normed=True)
+plt.plot(x_w, W(x_w)/I, color='r', linewidth=2)
+plt.show()
