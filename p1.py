3bfeccd734ba7d936d80de07f37c0e98d1d3e5d5
Fernanda Perez Verdugo <fer20mc@gmail.com>
2015-11-14 22:40:56 -0300

    se agrega semilla y datos random a utilizar

--- p1.py
+++ p1.py
@@ -12,3 +12,9 @@ var_w = 0.0
 var_wx = 0.0
 var_wy = 0.0
 var_wz = 0.0
+
+np.random.seed(212)
+n = (int)(1e5)
+x = np.random.uniform(-4, 4, n)
+y = np.random.uniform(-6, 6, n)
+z = np.random.uniform(-1, 1, n)
