15bf8c54b7bed890e68f8ba6050b5baa51bfe491
Fernanda Perez Verdugo <fer20mc@gmail.com>
2015-11-14 22:50:59 -0300

    se prueba caso densidad 1 y calcular centro de masa solo al toro

--- p1.py
+++ p1.py
@@ -18,3 +18,19 @@ n = (int)(1e5)
 x = np.random.uniform(-4, 4, n)
 y = np.random.uniform(-6, 6, n)
 z = np.random.uniform(-1, 1, n)
+
+for i in range(n):
+    if z[i]**2 + ( np.sqrt(x[i]**2 + y[i]**2) - 3 )**2 <=1:
+        suma_w += densidad
+        suma_wx += x[i] * densidad
+        suma_wy += y[i] * densidad
+        suma_wz += z[i] * densidad
+
+weight = volumen * suma_w / n
+x_momento = volumen * suma_wx / n
+y_momento = volumen * suma_wy / n
+z_momento = volumen * suma_wz / n
+
+print x_momento / weight
+print y_momento / weight
+print z_momento / weight
