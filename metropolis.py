b390af27a565c760a681f2f8240a1d6d177a76d9
nicokurtovic <nicokurtovic@gmail.com>
2015-11-15 17:52:48 -0300

    Errores STD del histograma por bin

--- metropolis.py
+++ metropolis.py
@@ -23,8 +23,6 @@ from scipy import integrate as sci
 #                                                                           #
 #############################################################################
 
-np.random.seed(251093)
-
 
 def w(x):
     '''
@@ -38,8 +36,8 @@ def w(x):
 def un_paso_metropolis(w, xn, d):
     '''
     Un paso segun algoritmo de Metropolis.
-    - Input: (funcion, semilla, distancia)
-    - Output: xn o xp, segun criterio metropolis
+    - Input: (funcion, semilla, distancia).
+    - Output: xn o xp, segun criterio metropolis.
     En base a un 'xn' dado evalua un 'xp' nuevo generado de manera random
     ubicado en un conjunto [xn - d, xn + d]. Si el xp cumple el criterio de
     Metropolis, retorna xp, en caso contrario retorna xn.
@@ -56,8 +54,8 @@ def un_paso_metropolis(w, xn, d):
 def generador(w, xn, N, d):
     '''
     Generador de funcion distribucion.
-    - Input: (funcion, xn semilla, Numero de bins, distancia pasos)
-    - Output: Arreglo 'y' distribuido segun w, porcentaje de pasos aceptados
+    - Input: (funcion, xn semilla, Numero de bins, distancia pasos).
+    - Output: Arreglo 'y' distribuido segun w, porcentaje de pasos aceptados.
     '''
     y = np.zeros(N)
     # Guardar la primera semilla
@@ -79,7 +77,7 @@ def calculador_d(dmin, dmax, N):
     '''
     Porcentaje de aceptados en funcion de la distancia de los pasos.
     - Input: (dmin, dmax, Numero pasos)
-    - Output: Arreglo de distancias 'D', Arreglo de porcentajes 'P'
+    - Output: Arreglo de distancias 'D', Arreglo de porcentajes 'P'.
     '''
     D = np.linspace(dmin, dmax, N)
     P = np.zeros(len(D))
@@ -89,35 +87,102 @@ def calculador_d(dmin, dmax, N):
     return D, P
 
 
+def calculador_error(w, N, n, l, d):
+    '''
+    Calculador de error en el histograma.
+    - Input: (funcion, N=numeros, n=iteraciones, largo hist, distancia).
+    - Output: Arreglo 'S' de desviaciones estandar en cada bin.
+    '''
+    z = 0
+    
+    Y = np.zeros([N,n])
+    for i in range(n):
+        xn = np.random.uniform(-2., 2.)
+        Y[:,i], porcentaje = generador(w, xn, N, d)
+        
+        z += 1
+        print z
+    
+    H = np.zeros([l-1,n])
+    
+    z = 0
+    
+    for i in range(n):
+        num, bins, patches = p.hist(Y[:,i], normed=1, bins=np.linspace(-10., 10., l))
+        H[:,i] = num
+        
+        z += 1
+        print z
+        
+    S = np.zeros(l-1)
+    for i in range(len(S)):
+        S[i] = np.std(H[i,:])
+    return bins, S
+
+
+def guardar_archivo(x, y, nombre):
+    '''
+    Crea un archivo 'nombre.dat' con dos columnas. No lleva header.
+    '''
+    arch = open(nombre + '.dat', 'w')
+    for i in range(len(x)):
+        arch.write(str(x[i])+' '+str(y[i])+'\n')
+    arch.close()
+
+
 #############################################################################
 #                                                                           #
 #############################################################################
 
 
+# Semilla
+np.random.seed(251093)
+
+'''
 # Encontrar d optimo. Se encuentra d = 3.96
 D, P = calculador_d(3., 5., 50)
 p.plot(D, P, '.')
 p.xlabel("Distancia pasos $d$")
 p.ylabel("Porcentaje aceptados")
 p.show()
-
+'''
 
 # Vectores a utilizar
-x = np.linspace(-10., 10., 10**6)
-y1 = w(x)
+x1 = np.linspace(-10., 10., 10**6)
+y1 = w(x1)
+'''
 y2, porcentaje = generador(w, 0., 10**7, 3.96)
 
 
 # Integral para calcular normalizacion
-integral = sci.trapz(y1, x=x)
+integral = sci.trapz(y1, x=x1)
 print integral
 
 
 # Ploteo
 fig1 = p.figure()
 fig1.clf()
-n, bins, patches = p.hist(y2, normed=1, bins=10**2)
-p.plot(x, y1, 'r')
+m, bins, patches = p.hist(y2, normed=1, bins=10**2)
+p.plot(x1, y1, 'r')
 p.xlabel("$x$")
 p.ylabel("$W(x)$")
+p.show()
+'''
+
+# calculador_error(w, N, n, l, d)
+#bins, S = calculador_error(w, 10**6, 100, 1000, 3.96)
+#x2 = bins[:-1]
+
+#guardar_archivo(x2, S, 'std')
+archivo = np.loadtxt('std.dat')  # Se abre el archivo
+
+x = archivo[:, 0]
+y = archivo[:, 1]
+
+fig2 = p.figure()
+fig2.clf()
+p.plot(x, y, '*')
+p.plot(x1, y1)
+p.xlabel("$x$")
+p.ylabel("$std(x)$")
 p.show()
