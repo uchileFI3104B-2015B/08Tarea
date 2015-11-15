7458a5811e018cd964a55229788b1f149e99877c
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-15 17:02:28 -0400

    inicializacion p1

---
+++ parte1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Este codigo determina una estimacion del centro de masas de un toro intersectado
+con un cilindro
+'''
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+#funciones esenciales
+
+
+def densidad(x, y, z):
+    '''
+    entrega la densidad del punto de coord (x, y, z)
+    '''
+    pass
+
+
+def en_cuerpo(x, y, z):
+    '''
+    verifica si el punto cumple la condicion de estar en el cuerpo
+    '''
+    pass
+
+
+#main
+#inicializacion
+
+
+#iteracion
