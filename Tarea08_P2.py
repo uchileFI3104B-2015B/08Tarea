7f34b124f23d4406d7d0049226ae2f60de73826a
Jorge Gacitua <jorge.gacitua@dgf.uchile.cl>
2015-11-15 20:29:35 -0300

    se agrega la funcion para necontrar el d bueno

--- Tarea08_P2.py
+++ Tarea08_P2.py
@@ -7,14 +7,15 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 
 
-def W(x, normalizacion):
+def W(x):
     '''
     Define la distribucion w(x)
     '''
-    distribuion = 3.5 * np.exp(- (x - 3.)**2 / 3.) + 2 * np.exp(- (x + 1.5)**2 / 0.5)
+    distribucion = 3.5 * np.exp(- (x - 3.)**2 / 3.) + 2 * np.exp(- (x + 1.5)**2 / 0.5)
     return distribucion
 
-def normalizacion(inicio,fin):
+
+def normalizacion(inicio, fin):
     '''
     calcula la normalizacion de la funcion entro los puntos inicio y fin
     para 10000 puntos
@@ -22,9 +23,40 @@ def normalizacion(inicio,fin):
     x = np.linspace(inicio, fin, 10000)
     integral = integrate.trapz(W(x), x=x)
 
-def avanza_metropolis(distribucion, xn, delta):
-    xp = xn + r * np.random.uniform(-1., 1.)
-    if W(xp)/W(xn) > np.random.uniform(0., 1.):
+
+def avanza_metropolis(xn, delta):
+    xp = xn + delta * np.random.uniform(-1., 1.)
+    if W(xp) / W(xn) > np.random.uniform(0., 1.):
         return xp
     else :
         return xn
+
+
+
+def encuentra_d_optimo(N_pruebas, d_minimo, d_maximo):
+    '''
+    Calcula el factor d para el cual se rechazan o aceptan
+    la mitad de las pruebas
+    '''
+    norm = normalizacion(-500, 500)
+    Deltas = np.linspace(d_minimo, d_maximo, N_pruebas)
+    Porcentaje = 0
+    datos = np.zeros(N_pruebas)
+    j = 0
+    while np.fabs(Porcentaje - 50.) >= 0.001:
+        aceptados = 0
+        rechazados = 0
+        for i in range(1, N_pruebas):
+            datos[i]=avanza_metropolis(datos[i-1], Deltas[j])
+            if datos[i]==datos[i-1]:
+                aceptados += 1
+            else:
+                rechazados += 1
+
+        Porcentaje = 100 * aceptados / N_pruebas
+        j += 1
+        print j
+
+    return Deltas[j]
+
+d=encuentra_d_optimo(1000, -10,10)
