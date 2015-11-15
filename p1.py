6e48425bc4404d614f4b42899428460c2b136ab2
fernanda perez <fernanda@MacBook-Pro-de-fernanda.local>
2015-11-15 15:19:33 -0800

    se calculan errores de las integrales con metodo de montecarlo, y luego errores de las coordenadas del CM con propagacion de errores de divisiones

--- p1.py
+++ p1.py
@@ -22,6 +22,7 @@ x = np.random.uniform(-4, 4, n)
 y = np.random.uniform(-6, 6, n)
 z = np.random.uniform(-1, 1, n)
 
+
 for i in range(n):
     if z[i]**2 + ( np.sqrt(x[i]**2 + y[i]**2) - 3 )**2 <=1:
         if (x[i]-2)**2 + z[i]**2 <=1:
@@ -29,14 +30,30 @@ for i in range(n):
             suma_wx += x[i] * densidad(x[i], y[i], z[i])
             suma_wy += y[i] * densidad(x[i], y[i], z[i])
             suma_wz += z[i] * densidad(x[i], y[i], z[i])
+            var_w += densidad(x[i], y[i], z[i])**2
+            var_wx += densidad(x[i], y[i], z[i])**2
+            var_wy += densidad(x[i], y[i], z[i])**2
+            var_wz += densidad(x[i], y[i], z[i])**2
 
 weight = volumen * suma_w / n
 x_momento = volumen * suma_wx / n
 y_momento = volumen * suma_wy / n
 z_momento = volumen * suma_wz / n
 
+# desviaciones estandar (error)
+desv_std_w = volumen * np.sqrt((var_w / n - (weight / n)**2) / n)
+desv_std_mom_x = volumen * np.sqrt((var_wx / n - (x_momento / n)**2) / n)
+desv_std_mom_y = volumen * np.sqrt((var_wy / n - (y_momento / n)**2) / n)
+desv_std_mom_z = volumen * np.sqrt((var_wz / n - (z_momento / n)**2) / n)
+
 x_cm = x_momento / weight
 y_cm = y_momento / weight
 z_cm = z_momento / weight
 
+# errores asociados (desviaciones estandar)
+err_x_cm = x_cm * np.sqrt((desv_std_mom_x/x_momento)**2+(desv_std_w/weight)**2)
+err_y_cm = y_cm * np.sqrt((desv_std_mom_y/y_momento)**2+(desv_std_w/weight)**2)
+err_z_cm = z_cm * np.sqrt((desv_std_mom_z/z_momento)**2+(desv_std_w/weight)**2)
+
 print x_cm, y_cm, z_cm
+print err_x_cm, err_y_cm, err_z_cm
