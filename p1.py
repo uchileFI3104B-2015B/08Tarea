47e511d9e1f98fc452b5da28caf573cee928a720
Fernanda Perez Verdugo <fer20mc@gmail.com>
2015-11-14 22:37:03 -0300

    se inicializa p1.py y se agregan variables a utilizar con caso basico densidad=1

---
+++ p1.py
@@ -0,0 +1,14 @@
+from __future__ import division
+import numpy as np
+
+densidad = 1
+volumen = 8.0 * 12.0 * 2.0
+
+suma_w = 0.0
+suma_wx = 0.0
+suma_wy = 0.0
+suma_wz = 0.0
+var_w = 0.0
+var_wx = 0.0
+var_wy = 0.0
+var_wz = 0.0
