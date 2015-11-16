1aea4186c0ff61b49182f699524d10d86e43e9c9
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-15 22:59:16 -0400

    detalles en la funcion
    se definen alguna de las funciones que se usaran

--- parte2.py
+++ parte2.py
@@ -15,13 +15,19 @@ def w(x):
     '''
     distribucion sin normalizar
     '''
+    w = 3.5 * np.exp(- (x - 3) ** 2 / 3) + 2 * np.exp(- (x + 1.5) ** 2 / 0.5)
+    return w
     pass
 
 
-def normalizar(x):
+def normalizar():
     '''
-    calcula la integral de la distribucion en todo el espacio
+    valor de la integral de la distribucion en todo el espacio
+    se propone el calculo numerico, pero por ahora se usa un valor dado por la
+    solucion en wolframalpha de
+    3.5 * exp(- (x - 3) ** 2 / 3) + 2 * exp(- (x + 1.5) ** 2 / 0.5)
     '''
+    return 13.2516
     pass
 
 
@@ -29,13 +35,18 @@ def W(x):
     '''
     distribucion normalizada
     '''
+    return w(x) / normalizar
     pass
 
 
-def cond_metropolis():
+def cond_metropolis(xn, d):
     '''
     aplica condicion de metropolis
     '''
+    xp = xn + d * np.random.uniform(- 1, 1)
+    if W(xp) / W(xn) > r:
+        xn = xp
+    return xn
     pass
 
 
