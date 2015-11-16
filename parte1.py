3d6f88177bf78c7fde297c9483f16e58b70a13b2
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-15 20:59:07 -0400

    refactoring
    se fija semilla para verificar resultados y que el refactoring este bien
    hecho

--- parte1.py
+++ parte1.py
@@ -10,6 +10,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(0)
 #funciones esenciales
 
 
@@ -36,49 +37,58 @@ def en_cuerpo(x, y, z):
     return (en_toro(x, y, z) and en_cilindro(x, y, z))
 
 
+def varianza(V, n, m, vm):
+    return V * np.sqrt(np.fabs((vm / n - (m / n) ** 2) / n))
+
+
+def error(M, VM, Mi, VMi):
+    '''
+    calcula el error con el tratamiento adecuado
+    '''
+    return (Mi / M) * np.sqrt((VMi / Mi) ** 2 + (VM / M) ** 2)
+
+
+def centro_masa(V, n):
+    mx = 0; my = 0; mz = 0; m = 0
+    vmx = 0; vmy = 0; vmz = 0; vm = 0
+    for i in range(0, n):
+        x = 2 + 1 * np.random.uniform(0., 1.)
+        y = - 4 + 8 * np.random.uniform(0., 1.)
+        z = - 1 + 2 * np.random.uniform(0., 1.)
+        if en_cuerpo(x, y, z):
+            m += densidad(x, y, z)
+            mx += x * densidad(x, y, z)
+            my += y * densidad(x, y, z)
+            mz += z * densidad(x, y, z)
+            vm += (densidad(x, y, z)) ** 2
+            vmx += (x * densidad(x, y, z)) ** 2
+            vmy += (y * densidad(x, y, z)) ** 2
+            vmz += (z * densidad(x, y, z)) ** 2
+    M = V * m / n
+    Mx = V * mx / n
+    My = V * my / n
+    Mz = V * mz / n
+    VM = varianza(V, n, m, vm)
+    VMx = varianza(V, n, mx, vmx)
+    VMy = varianza(V, n, my, vmy)
+    VMz = varianza(V, n, mz, vmz)
+    Xcm = Mx / M
+    Ycm = My / M
+    Zcm = Mz / M
+    VXcm = error(M, VM, Mx, VMx)
+    VYcm = error(M, VM, My, VMy)
+    VZcm = error(M, VM, Mz, VMz)
+    CM = Xcm, Ycm, Zcm
+    VCM = VXcm, VYcm, VZcm
+    return CM, VCM
+
 # main
 # inicializacion
 n = 100
-mx = 0
-my = 0
-mz = 0
-m = 0
-vmx = 0
-vmy = 0
-vmz = 0
-vm = 0
 dx = 1.; dy = 8.; dz = 2.
 V = dx * dy * dz
-# iteracion
-for i in range(0, n):
-    x = 2 + 1 * np.random.uniform(0., 1.)
-    y = - 4 + 8 * np.random.uniform(0., 1.)
-    z = - 1 + 2 * np.random.uniform(0., 1.)
-    if en_cuerpo(x, y, z):
-        m += densidad(x, y, z)
-        mx += x * densidad(x, y, z)
-        my += y * densidad(x, y, z)
-        mz += z * densidad(x, y, z)
-        vm += (densidad(x, y, z)) ** 2
-        vmx += (x * densidad(x, y, z)) ** 2
-        vmy += (y * densidad(x, y, z)) ** 2
-        vmz += (z * densidad(x, y, z)) ** 2
+r, vr = centro_masa(V, n)
 
 
-M = V * m / n
-Mx = V * mx / n
-My = V * my / n
-Mz = V * mz / n
-VM = V * np.sqrt(np.fabs((vm / n - (m / n) ** 2) / n))
-VMx = V * np.sqrt(np.fabs((vmx / n - (mx / n) ** 2) / n))
-VMy = V * np.sqrt(np.fabs((vmy / n - (my / n) ** 2) / n))
-VMz = V * np.sqrt(np.fabs((vmz / n - (mz / n) ** 2) / n))
-Xcm = Mx / M
-Ycm = My / M
-Zcm = Mz / M
-VXcm = (Mx / M) * np.sqrt((VMx / Mx) ** 2 + (VM / M) ** 2)
-VYcm = (My / M) * np.sqrt((VMy / My) ** 2 + (VM / M) ** 2)
-VZcm = (Mz / M) * np.sqrt((VMz / Mz) ** 2 + (VM / M) ** 2)
-print VXcm
-print VYcm
-print VZcm
+print r
+print vr
