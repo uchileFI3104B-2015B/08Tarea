1e4e436efaa4764c8c9a0bb2ad1911cdb969c057
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-15 22:32:08 -0400

    inicializacion p2

---
+++ parte2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+
+'''
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+# funciones estructurales
+
+def w(x):
+    '''
+    distribucion sin normalizar
+    '''
+    pass
+
+
+def normalizar(x):
+    '''
+    calcula la integral de la distribucion en todo el espacio
+    '''
+    pass
+
+
+def W(x):
+    '''
+    distribucion normalizada
+    '''
+    pass
+
+
+def cond_metropolis():
+    '''
+    aplica condicion de metropolis
+    '''
+    pass
+
+
+def d_optimo():
+    '''
+    busca el paso optimo
+    '''
+    pass
+
+
+# inicializacion
+
+
+# iteracion
