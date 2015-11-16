4a575b31be1d4b5fa4595f1006ae7efa4a03ca56
maxidirk <maxidirk@yahoo.es>
2015-11-15 21:13:18 -0300

    se añaden algunos comentarios y se hacen correcciones menores

--- Tarea8.py
+++ Tarea8.py
@@ -16,7 +16,7 @@ import scipy.integrate as scint
 
 ##############################################################################
 ##############################################################################
-# funciones
+# funciones P1
 
 def toro(x,y,z):
     '''
@@ -38,13 +38,13 @@ def rho(x,y,z):
 
 def integrar_montecarlo_cuerpo(rho,N,V):
     '''
-    Integra la funcion rho dentro de una caja de volumen 2*10*2=V
+    Integra la funcion rho dentro del cuerpo en una caja de volumen 2*10*2=V
     usando el metodo de montecarlo con N muestras.
     Entrega la masa total dentro del cuerpo formado por el toro y el cilindro
     y la posicion del centro de masa en (x, y, z)
     '''
     x = np.random.uniform(1,3,N)   # selecciona punto x dentro de V
-    y = np.random.uniform(-5,5,N)  # selecciona punto y dentro de V
+    y = np.random.uniform(-4,4,N)  # selecciona punto y dentro de V
     z = np.random.uniform(-1,1,N)  # selecciona punto z dentro de V
     m = 0.
     xm = 0.
@@ -68,6 +68,14 @@ def integrar_montecarlo_cuerpo(rho,N,V):
     return M, CMx, CMy, CMz, dentro, fuera
 
 ##############################################################################
+# funciones P2
+
+def w(x):
+    W = 3.5 * np.exp((-(x - 3.)**2) / 3.) + 2. * np.exp((-(x + 1.5)**2) / 0.5)
+    return W
+
+
+##############################################################################
 ##############################################################################
 # P1
 '''
@@ -76,16 +84,17 @@ radio mayor 4 y radio menor 2
 cilindro : (x-2)**2 + z**2 <= 1
 rho(x,y,z) = 0.5*(x**2 + y**2 + z**2)
 estimar posicion del centro de masa
-pag304(328)
 rcm= 1/M * int(r*rho(r)dV)
-integrar en x y z separadamente
+Caja mas pequenha que contiene al cuerpo ([1,3],[-4,4],[-1,1])
 '''
 #np.random.seed(24)
 
 N = 1e5     # numero de muestras
-V = 2.*10.*2.  # volumen de una caja que contiene el cuerpo
+V = 2.*8.*2.  # volumen de una caja que contiene el cuerpo
+R = integrar_montecarlo_cuerpo(rho,N,V)  # se resuelve el problema
 
-print integrar_montecarlo_cuerpo(rho,N,V)
+print 'Masa total de cuerpo =', R[0]
+print 'Pocion del centro de masa = (', R[1],',',R[2],',',R[3],')'
 
 ##############################################################################
 ##############################################################################
@@ -96,7 +105,7 @@ xp = xn +d*r
 r es uniforme(-1,1)
 generar muestra de unos 10 millones de puntos
 '''
-w = lambda x: 3.5 * np.exp((-(x - 3.)**2) / 3.) + 2. * np.exp((-(x + 1.5)**2) / 0.5)
+
 
 ##############################################################################
 ##############################################################################
