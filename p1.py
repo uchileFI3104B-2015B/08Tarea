e145f1357eb51417cea5fea86506aaeeaf8851e5
TatiFlores <tatiflores.4@gmail.com>
2015-11-16 11:13:03 -0300

    agrega funciones para saber si se esta dentro del toro o cilindro

--- p1.py
+++ p1.py
@@ -5,7 +5,19 @@
 Aquí va el docstring de mi codigo explicando que cosas hace
 '''
 
+
 def densidad(x, y, z):
     ''' Retorna la densidad del sólido en una coordenada dada '''
     rho = 0.5 + (x**2 + y**2 + z**2)
     return rho
+
+
+def dentro_toro(x, y, z):
+    '''Retorna True si las coordenadas están dentro del toro,
+    y False si no lo está'''
+    return z**2 + (sqrt(x**2 + y**2) - 3)**2 <= 1
+
+def dentro_cilindro(x, y, z):
+    '''Retorna True si las coordenadas están dentro del toro,
+    y False si no lo está'''
+    return (x-2)**2 + z**2 <= 1
