a632027569a8076608e2737426c83de731de8a66
constanzaurzua <urzuacisterna@gmail.com>
2015-11-17 16:05:10 -0300

    cambio de funciones y se agrega los return

--- t8.py
+++ t8.py
@@ -12,3 +12,8 @@ from __future__ import division
 def densidad(x,y,z):
 ''' retorna el valor de la densidad'''
     return 0.5*(x**2 + y**2 + z**2)
+
+
+def esta_en_toro(x,y,z):
+    '''define si las coordenadas (x,y,z) estan en el toro, retorna verdadero o falso '''
+    return z**2 + (np.sqrt(x**2 + y**2)-3) <= 1
