350368a6edf56db168b9e8cae014a4287243f7b5
Martín!!! <martin_panza_p@hotmail.com>
2015-11-17 16:25:56 -0300

    se hace el setup

--- Pregunta1.py
+++ Pregunta1.py
@@ -9,3 +9,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+
+np.random.seed(7000)
+N = 100000
+rand = np.random.uniform(low=0.0, high=1.0, size=N*3+3)
