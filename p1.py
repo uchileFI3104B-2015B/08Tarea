31a9b87ad78ef0d599666446370f4d5a7f2e11f4
TatiFlores <tatiflores.4@gmail.com>
2015-11-16 21:03:03 -0300

    termina funcion montecarlo

--- p1.py
+++ p1.py
@@ -21,7 +21,7 @@ def dentro_toro(x, y, z):
 def dentro_cilindro(x, y, z):
     '''Retorna True si las coordenadas están dentro del cilindro,
     y False si no lo está'''
-    return (x-2)**2 + z**2 <= 1
+    return (x - 2)**2 + z**2 <= 1
 
 
 def interseccion(x, y, z):
@@ -30,5 +30,48 @@ def interseccion(x, y, z):
     return dentro_toro(x, y, z) and dentro_cilindro(x, y, z)
 
 
-def MC1():
+def MC1(N):
     ''' Utiliza el algoritmo de Montecarlo 1 para bla'''
+    # N = Numero de puntos
+    volumen = 3 * 8 * 2. # intervalo x * intervalo y * intervalo z
+
+    suma_peso = 0
+    suma_x = 0
+    suma_y = 0
+    suma_z = 0
+
+    var_peso = 0
+    var_x = 0
+    var_y = 0
+    var_z = 0
+
+    for i in range(N):
+        x = np.random.uniform(1, 4)
+        y = np.random.uniform(-4, 4)
+        z = np.random.uniform(-1, 1)
+
+        if dentro_toro(x, y, z):
+            suma_peso += densidad(x, y, z)
+            suma_x += x * densidad(x, y, z)
+            suma_y += y * densidad(x, y, z)
+            suma_z += z * densidad(x, y, z)
+            var_peso += densidad(x, y, z)**2
+            var_x += (densidad(x, y, z) * x)**2
+            var_y += (densidad(x, y, z) * y)**2
+            var_z += (densidad(x, y, z) * z)**2
+
+    integral_peso = volumen * suma_peso / N
+    integral_x = volumen * suma_x / N
+    integral_y = volumen * suma_y / N
+    integral_z = volumen * suma_z / N
+
+    # Errores asociados
+    err_peso = volumen * np.sqrt((var_peso / (N - suma_peso)**2 / N) / N)
+    err_x = volumen * np.sqrt((var_x / N - (suma_x / N)**2) / N)
+    err_y = volumen * np.sqrt((var_y / N - (suma_y / N)**2) / N)
+    err_z = volumen * np.sqrt((var_z / N - (suma_z / N)**2) / N)
+
+    Integrales = [integral_peso, integral_x, integral_y, integral_z]
+    Errores = [err_peso, err_x, err_y, err_z]
+
+    return [Integrales, Errores]
