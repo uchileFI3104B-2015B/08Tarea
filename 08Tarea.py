3e2b3a8b260f65b40416d33b4d707a120c80fb4e
[Felipe Castillo] <felipe.castillo.t@ing.uchile.cl>
2015-11-17 05:19:00 -0300

    Iniciacion del script que resuelve p1

---
+++ 08Tarea.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+'''
+este script utiliza el algoritmo de monte carlo para encontrar el centro de masa de un volumen generado
+por la intersección de un toroide y un cilindro
+'''
