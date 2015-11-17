1e3396026606348a5d2a124596f76c2ee1894a13
Martín!!! <martin_panza_p@hotmail.com>
2015-11-17 16:25:03 -0300

    se crea archivo y se realizan las importaciones

---
+++ Pregunta1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 16:23:27 2015
+
+@author: splatt
+"""
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import math
