048808c1881b386ce6ccb296e19d74b02a792520
benjammin94 <benja_hunter_x@hotmail.com>
2015-11-17 19:05:26 -0300

    Update 3
    Se agregan errores.

--- codigo1.py
+++ codigo1.py
@@ -27,16 +27,19 @@ def var(x, y, z):
     return (vw, vx, vy, vz)
 
 sw=0
-sx=0
-sy=0
-sz=0
+swx=0
+swy=0
+swz=0
+
 varw=0
 varx=0
 vary=0
 varz=0
+
 V=2*8*2
-n=1000000
-for i in range(0, n):
+n=1e6
+
+for i in range(0, int(n)):
     x = 1 + 3 * np.random.uniform(0., 1.)
     y = - 4 + 8 * np.random.uniform(0., 1.)
     z = - 1 + 2 * np.random.uniform(0., 1.)
@@ -44,19 +47,28 @@ for i in range(0, n):
         var_i=var(x, y, z)
         den_i=densidad(x, y, z)
         sw += densidad(x, y, z)
-        sx += x * den_i
-        sy += y * den_i
-        sz += z * den_i
+        swx += x * den_i
+        swy += y * den_i
+        swz += z * den_i
         varw += var_i[0]
         varx += var_i[1]
         vary += var_i[2]
         varz += var_i[3]
 
 w = V * sw / n
-x = V * sx / n / w
-y = V * sy / n / w
-z = V * sz / n / w
+x = V * swx / n / w
+y = V * swy / n / w
+z = V * swz / n / w
 
-print x
-print y
-print z
+dw = V * ((varw / n - (sw / n)) / n) ** 0.5
+dx = V * ((varx / n - (swx / n)) / n) ** 0.5
+dy = V * ((vary / n - (swy / n)) / n) ** 0.5
+dz = V * ((varz / n - (swz / n)) / n) ** 0.5
+
+d_cm_x= (x / w) * (((dw / w) ** 2 + (dx / x) ** 2) ** 0.5)
+d_cm_y= (y / w) * (((dw / w) ** 2 + (dy / y) ** 2) ** 0.5)
+d_cm_z= (z / w) * (((dw / w) ** 2 + (dz / z) ** 2) ** 0.5)
+
+print 'Centro de masa en x =', x, '+-', d_cm_x
+print 'Centro de masa en y =', y, '+-', d_cm_y
+print 'Centro de masa en z =', z, '+-', d_cm_z
