63dd3ea90ad68461e9a5ee8361a29d5d9e129651
benjammin94 <benja_hunter_x@hotmail.com>
2015-11-18 00:18:31 -0300

    Update 6
    Aprueba pep8. Agrega imagen.

--- codigo1.py
+++ codigo1.py
@@ -1,51 +1,64 @@
 '''
-Este script blablabla
+Este script estima numericamente las coordenadas del centro de masa del cuerpo
+producido por la interseccion de un
+Toro: z ** 2 + ((x **2 + y**2) ** 0.5 - 3)**2 <= 1
+y de un
+Cilindro: (x - 2) ** 2 + z ** 2 <= 1
+con una distribucion de densidad dada por 0.5 * (x ** 2 + y ** 2 + z **2)
 '''
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Designamos la semilla
 np.random.seed(2308)
 
 
 def en_cuerpo(x, y, z):
-    #True si esta dentro del cuerpo (Toro and Cilindro), False si no
-    return (z ** 2 + ((x **2 + y**2) ** 0.5 - 3)**2 <= 1 and
+    # True si esta dentro del cuerpo (Toro and Cilindro), False si no
+    return (z ** 2 + ((x ** 2 + y ** 2) ** 0.5 - 3) ** 2 <= 1 and
             (x - 2) ** 2 + z ** 2 <= 1)
 
 
 def densidad(x, y, z):
-    return 0.5 * (x ** 2 + y ** 2 + z **2)
+    # Entrega el valor de la densidad para los distintos valores de x, y, z
+    return 0.5 * (x ** 2 + y ** 2 + z ** 2)
 
 
 def var(x, y, z):
+    # Determina varianza de masa, x, y, z
     vw = densidad(x, y, z) ** 2
     vx = (densidad(x, y, z) * x) ** 2
     vy = (densidad(x, y, z) * y) ** 2
     vz = (densidad(x, y, z) * z) ** 2
     return (vw, vx, vy, vz)
 
-sw=0
-swx=0
-swy=0
-swz=0
 
-varw=0
-varx=0
-vary=0
-varz=0
+# Main
+# Setup
 
-V=2*8*2
-n=1e6
+# Inicializo valores
+sw = 0
+swx = 0
+swy = 0
+swz = 0
+
+varw = 0
+varx = 0
+vary = 0
+varz = 0
+
+# Volumen:
+V = 2*8*2
+n = 1e6
 
 for i in range(0, int(n)):
     x = 1 + 3 * np.random.uniform(0., 1.)
     y = - 4 + 8 * np.random.uniform(0., 1.)
     z = - 1 + 2 * np.random.uniform(0., 1.)
     if en_cuerpo(x, y, z):
-        var_i=var(x, y, z)
-        den_i=densidad(x, y, z)
+        var_i = var(x, y, z)
+        den_i = densidad(x, y, z)
         sw += densidad(x, y, z)
         swx += x * den_i
         swy += y * den_i
@@ -55,20 +68,23 @@ for i in range(0, int(n)):
         vary += var_i[2]
         varz += var_i[3]
 
-w = V * sw / n
-x = V * swx / n / w
-y = V * swy / n / w
-z = V * swz / n / w
+w = V * sw / n          # Valor promedio de la masa
+x = V * swx / n / w     # Valor promedio de coordenada x
+y = V * swy / n / w     # Valor promedio de coordenada y
+z = V * swz / n / w     # Valor promedio de coordenada z
 
+# Errores asociados
 dw = V * ((varw / n - (sw / n)) / n) ** 0.5
 dx = V * ((varx / n - (swx / n)) / n) ** 0.5
 dy = V * ((vary / n - (swy / n)) / n) ** 0.5
 dz = V * ((varz / n - (swz / n)) / n) ** 0.5
 
-d_cm_x= (x / w) * (((dw / w) ** 2 + (dx / x) ** 2) ** 0.5)
-d_cm_y= (y / w) * (((dw / w) ** 2 + (dy / y) ** 2) ** 0.5)
-d_cm_z= (z / w) * (((dw / w) ** 2 + (dz / z) ** 2) ** 0.5)
+# Errores asociados a los centro de masa
+d_cm_x = (x / w) * (((dw / w) ** 2 + (dx / x) ** 2) ** 0.5)
+d_cm_y = (y / w) * (((dw / w) ** 2 + (dy / y) ** 2) ** 0.5)
+d_cm_z = (z / w) * (((dw / w) ** 2 + (dz / z) ** 2) ** 0.5)
 
+# Muestra valores
 print 'Centro de masa en x =', x, '+-', d_cm_x
 print 'Centro de masa en y =', y, '+-', d_cm_y
 print 'Centro de masa en z =', z, '+-', d_cm_z
--- codigo2.py
+++ codigo2.py
@@ -1,19 +1,30 @@
 '''
+Este script genera una muestra aleatoria de numeros dada por la distribucion
+w(x) = 3.5 * np.exp(-(x - 3.)**2 / 3.) + 2. * np.exp(-2 * (x + 1.5)**2)
+utilizando el metodo de metropolis y los muestra en un grafico tipo histograma.
 '''
 
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+# Designamos la semilla
 np.random.seed(1994)
 
 
 def w(x):
-    return 3.5 * np.exp(-(x - 3.)**2 / 3.) + 2. * np.exp(-2 * (x + 1.5)**2)
+    # Distribucion sin normalizar
+    return 3.5 * np.exp(-(x - 3.) ** 2 / 3.) + 2. * np.exp(-2 * (x + 1.5) ** 2)
+
 
 def W(x):
+    # Distribucion normalizada
     return w(x) / norma
 
+
 def metropolis(w, xn, d):
+    # Algoritmo de metropolis
     r = np.random.uniform(-1., 1.)
     xp = xn + d * r
     c = np.random.uniform(0., 1.)
@@ -22,53 +33,64 @@ def metropolis(w, xn, d):
     else:
         return xn
 
+
 def porcentaje_acepta(w, xn, N, d):
+    # Entrega valores de porcentaje aceptados para cada delta
     xn_mas_1 = np.zeros(N)
     xn_mas_1[0] = xn
     acepto = 0.
     rechazo = 0.
-    for i in range (len(xn_mas_1) - 1):
+    for i in range(len(xn_mas_1) - 1):
         xn_mas_1[i+1] = metropolis(w, xn_mas_1[i], d)
         if xn_mas_1[i+1] == xn_mas_1[i]:
             rechazo += 1.
         else:
             acepto += 1.
     porcentaje = 100.0 * acepto / (acepto + rechazo)
-    return porcentaje
+    return porcentaje, xn_mas_1
 
 
-#Main
-#Set up
-N=100
-#Probamos delta
-n=30
+# Main
+# Set up
 
+# Inicializo valores
+N = 100
+n = 30
 xn_0 = 0
-
 d_posible = np.linspace(1., 10., n)
 porcentaje = np.zeros(n)
+
+# Iteramos para mostrar los porcentajes aceptados
 for i in range(n):
-    porcentajes = porcentaje_acepta(w, xn_0, N, d_posible[i])
+    porcentajes, xn_mas_1 = porcentaje_acepta(w, xn_0, N, d_posible[i])
     porcentaje[i] = np.copy(porcentajes)
 
+# Establecemos condicion de porcentaje = 50% (aprox) para estimar el delta
 for i in range(n):
     if porcentaje[i] >= 49 and porcentaje[i] <= 51:
         d = d_posible[i]
         break
-print d
+
+print d  # Se estimó d=4.72
 
 
-
-x_int=np.linspace(-10, 10, 1000)
+# Calculo la integral de w para normalizar
+x_int = np.linspace(-10, 10, 1000)
 norma = np.trapz(w(x_int), x_int)
 
-fig2 = plt.figure(2)
-fig2.clf()
-ax2 = fig2.add_subplot(111)
-plt.plot(d_posible, porcentaje, '-o', color='r')
-ax2.set_xlabel("Valor posible de $\delta$")
-ax2.set_ylabel("Porcentaje de veces aceptado")
-plt.axhline(y=50, color='g')
-plt.axvline(x=3.79, linewidth=1, color='0')
+# Plot W(x) (normalizado)
+fig = plt.figure(1)
+fig.clf()
+ax = fig.add_subplot(111)
+plt.plot(x_int, W(x_int), color='r', label="Distribucion deseada")
+ax.set_xlabel("x")
+ax.set_ylabel("W(x)")
+
+# Plot Histograma
+porcentaje_hist, xn_1_hist = porcentaje_acepta(w, xn_0, 1e7, d)
+n, bins, patches = plt.hist(xn_1_hist, bins=100, normed=1)
+
+plt.legend()
+plt.savefig('w_x.png')
 plt.show()
 plt.draw()
