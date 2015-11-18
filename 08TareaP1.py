002aae7c8e476a06486b8dfbe9ddfa6a4aa3b71c
[Felipe Castillo] <felipe.castillo.t@ing.uchile.cl>
2015-11-17 22:30:07 -0300

    Los codigos pasan pep8, creacion de imagen histograma

--- 08TareaP1.py
+++ 08TareaP1.py
@@ -1,4 +1,4 @@
-	#!/usr/bin/python
+#!/usr/bin/python
 # -*- coding: iso-8859-1 -*-
 import numpy as np
 import matplotlib
--- 08TareaP2.py
+++ 08TareaP2.py
@@ -1,4 +1,4 @@
-	#!/usr/bin/python
+#!/usr/bin/python
 # -*- coding: iso-8859-1 -*-
 from __future__ import division
 import numpy as np
@@ -8,43 +8,47 @@ import matplotlib.pyplot as plt
 este script utiliza el algoritmo de Metrópolis
 para obtener una muestra  aleatoria de numeros
 con la distribucion :
-w=3.5*exp((-(x-3)^2)/3.) + 2 exp((-(x+1.5)^2)/0.5)
+w=3.5*exp((-(x-3)**2)/3.) + 2 exp((-(x+1.5)**2)/0.5)
 '''
 
 
-def MetropolisP2(n,d,xn):
+def MetropolisP2(n, d, xn):
     for i in range(n):
-        r=-1+2*np.random.sample()
+        r = -1+2*np.random.sample()
         xp = xn + (d*r)
-        wxp=3.5*np.exp((-(xp-3)**2)/3.) + 2*np.exp(-((xp+1.5)**2)/0.5)
-        wxn=3.5*np.exp((-(xn-3)**2)/3.) + 2*np.exp(-((xn+1.5)**2)/0.5)
-        if (wxp / wxn) > np.random.sample(): #problema con positivos
-
-            xn_plus=xp
+        wxp = 3.5*np.exp((-(xp-3)**2)/3.) + 2*np.exp((-(xp+1.5)**2)/0.5)
+        wxn = 3.5*np.exp((-(xn-3)**2)/3.) + 2*np.exp((-(xn+1.5)**2)/0.5)
+        if (wxp/wxn) > np.random.sample():
+            xn_plus = xp
             puntos.append(xn_plus)
         else:
-            xn_plus=xn
-        xn=xn_plus
+            xn_plus = xn
+            puntos.append(xn_plus)
+        xn = xn_plus
 
-#main
-#setup
+# main
+# setup
 
-puntos=[]
-n=1000000
-np.random.seed(199)
-xn=-1+2*np.random.sample()
-d=2
-MetropolisP2(n,d,xn)
-print "El criterio esta aceptando el", (len(puntos)/n)*100, "% de los puntos" #puntos
-#Plot Histograma
+puntos = []
+n = 10000000
+np.random.seed(19)  # semilla
+xn = -1+2*np.random.sample()  # valor inicial, depende de la semilla
+d = 4
+MetropolisP2(n, d, xn)
 
+# Plot Histograma
+
+integral = np.sqrt(np.pi)*(3.5*np.sqrt(3)+2*np.sqrt(0.5))
 num_bins = 100
-n, bins, patches = plt.hist(puntos, num_bins, normed=1, facecolor='green', alpha=0.5)
-
+n, bins, patches = plt.hist(puntos,
+                            num_bins, normed=True, facecolor='green')
+x = np.linspace(bins[0], bins[-1], 10000)
+w = 3.5*np.exp((-(x-3)**2)/3.) + 2*np.exp((-(x+1.5)**2)/0.5)
+plt.figure(1)
+plt.plot(x, w/integral, 'r', label='Distribucion W(x)')
 plt.xlabel('x')
-plt.ylabel('w')
-plt.title(r'Histograma')
-
-
-plt.subplots_adjust(left=0.15)
+plt.ylabel('w(x)')
+plt.title('Histograma')
+plt.legend()
+plt.savefig('Histograma.jpg')
 plt.show()
