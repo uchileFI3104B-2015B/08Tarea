652201866096c54d8320eaaa841da214392139fe
brevadiaz <eva.diaz.g@ing.uchile.cl>
2015-11-18 02:12:51 -0300

    P2 pasa pep8

--- p2tarea8.py
+++ p2tarea8.py
@@ -10,10 +10,12 @@ import matplotlib.pyplot as plt
 
 np.random.seed(456)
 
+
 def W(x):
     w = (3.5 * np.exp(-(x - 2)**2 / 3) +
          2 * np.exp(-(x + 1.5)**2 / 0.5))
     return w
+
 
 def prueba(x):
     r = np.random.uniform(-1, 1)
@@ -45,8 +47,10 @@ print(N_aceptados)
 plt.clf()
 plt.figure(1)
 x_values = np.linspace(-10, 10, 1000)
-plt.hist(x, bins=100, range=[-10, 10], normed=True, color='c', label="Distribucion obtenida")
-plt.plot(x_values, W(x_values)/norma, color='r', linewidth=2, label="Distribucion deseada")
+plt.hist(x, bins=100, range=[-10, 10], normed=True,
+         color='c', label="Distribucion obtenida")
+plt.plot(x_values, W(x_values)/norma, color='r',
+         linewidth=2, label="Distribucion deseada")
 plt.xlim([-10, 10])
 plt.ylim([0, 0.3])
 plt.xlabel('$x$')
