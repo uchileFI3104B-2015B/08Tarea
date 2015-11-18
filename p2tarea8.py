288265dbd32c09bfc7a11716b37141768c622726
brevadiaz <eva.diaz.g@ing.uchile.cl>
2015-11-17 23:56:20 -0300

    Termino la P2

--- p2tarea8.py
+++ p2tarea8.py
@@ -1,17 +1,51 @@
+'''
+Este script genera una muestra de puntos con distribución
+W(x) = (3.5 * np.exp(-(x - 2)**2 / 3) + 2 * np.exp(-(x + 1.5)**2 / 0.5))
+mediante el algoritmo de Metropolis.
+'''
+
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(456)
 
 def W(x):
     w = (3.5 * np.exp(-(x - 2)**2 / 3) +
          2 * np.exp(-(x + 1.5)**2 / 0.5))
     return w
 
-def prop(x):
+def prueba(x):
     r = np.random.uniform(-1, 1)
     xp = x + r * delta
     return xp
 
-delta = raw_input("Ingresar delta:")
-semilla = raw_input("Ingresar semilla:")
+# Main
+# Setup
+
+delta = 2
 N = 10000000
+N_aceptados = 0
+norma = np.sqrt(np.pi) * (3.5 * np.sqrt(3) + 2 * np.sqrt(0.5))
+x = np.zeros(N + 1)
+
+# Algoritmo de Metropolis
+for i in range(N):
+    h = np.random.uniform(0, 1)
+    xp = prueba(x[i])
+    if (W(xp) / W(x[i])) > h:
+        x[i+1] = xp
+        N_aceptados += 1
+    else:
+        x[i+1] = x[i]
+
+# Plot
+plt.clf()
+plt.figure(1)
+x_values = np.linspace(-10, 10, 1000)
+plt.hist(x, bins=100, range=[-10, 10], normed=True, color='c', label="Distribucion obtenida")
+plt.plot(x_values, W(x_values)/norma, color='g', linewidth=4, label="Distribucion deseada")
+plt.xlim([-10, 10])
+plt.ylim([0, 1])
+plt.savefig('grafico1.png')
+plt.show()
